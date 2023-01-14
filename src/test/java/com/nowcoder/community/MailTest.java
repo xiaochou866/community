147af0bf5ef32c3248a40dfdd329bc19f6d6d3d0
Joker <1574427203@qq.com>
2023-01-14 17:45:19 +0800

    使用QQ邮箱完成邮件发送功能

---
+++ src/test/java/com/nowcoder/community/MailTest.java
@@ -0,0 +1,34 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.util.MailClient;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+
+@SpringBootTest
+public class MailTest {
+    @Autowired
+    private MailClient mailClient;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Test
+    public void testTextMail(){
+        mailClient.sendMail("209701828@qq.com","TEST", "欢迎");
+    }
+
+    @Test
+    public void testHtmlMail(){
+        Context context = new Context();
+        context.setVariable("username", "sunday");
+
+        String content = templateEngine.process("/mail/demo", context);
+        System.out.println(content);
+
+        mailClient.sendMail("209701828@qq.com","TEST", content);
+    }
+}
