a017ea6b435cd51518fc9b7d76e1e476205d5d31
Joker <1574427203@qq.com>
2023-01-15 20:40:23 +0800

    完成页面登录功能

---
+++ src/main/java/com/nowcoder/community/dao/LoginTicketMapper.java
@@ -0,0 +1,32 @@
+package com.nowcoder.community.dao;
+
+import com.nowcoder.community.entity.LoginTicket;
+import org.apache.ibatis.annotations.*;
+
+@Mapper
+public interface LoginTicketMapper {
+
+    //这里允许写多个字符串可以进行拼接 当你传入的是一个对象的时候Mybatis也会自动进行解包
+    @Insert({
+            "insert into login_ticket(user_id, ticket, status, expired) ",
+            "values(#{userId}, #{ticket}, #{status}, #{expired})"
+    })
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    int insertLoginTicket(LoginTicket loginTicket);
+
+    @Select({
+            "select id, user_id, ticket, status, expired ",
+            "from login_ticket where ticket=#{ticket}"
+    })
+    LoginTicket selectByTicket(String ticket);
+
+    @Update({
+            "<script>",
+            "update login_ticket set status=${status} where ticket=#{ticket} ",
+            "<if test=\"ticket!=null\">",
+            "and 1=1",
+            "</if>",
+            "</script>"
+    })
+    int updateStatus(String ticket, int status);
+}
--- src/test/java/com/nowcoder/community/MapperTests.java
+++ src/test/java/com/nowcoder/community/MapperTests.java
@@ -1,8 +1,10 @@
 package com.nowcoder.community;
 
 import com.nowcoder.community.dao.DiscussPostMapper;
+import com.nowcoder.community.dao.LoginTicketMapper;
 import com.nowcoder.community.dao.UserMapper;
 import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.entity.LoginTicket;
 import com.nowcoder.community.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +19,9 @@ public class MapperTests {
     private UserMapper userMapper;
     @Autowired
     private DiscussPostMapper discussPostMapper;
+
+    @Autowired
+    LoginTicketMapper loginTicketMapper;
 
     @Test
     public void testSelectUser(){
@@ -66,5 +71,26 @@ public class MapperTests {
 
         int rows = discussPostMapper.selectDiscussPostRows(0);
         System.out.println(rows);
+    }
+
+    @Test
+    public void testInsertLoginTest(){
+        LoginTicket loginTicket = new LoginTicket();
+        loginTicket.setUserId(101);
+        loginTicket.setTicket("abc");
+        loginTicket.setStatus(0);
+        loginTicket.setExpired(new Date(System.currentTimeMillis() + 1000*60*10));
+
+        loginTicketMapper.insertLoginTicket(loginTicket);
+    }
+
+    @Test
+    public void testSelectLoginTicket(){
+        LoginTicket loginTicket = loginTicketMapper.selectByTicket("abc");
+        System.out.println(loginTicket);
+
+        loginTicketMapper.updateStatus("abc", 1);
+        loginTicket = loginTicketMapper.selectByTicket("abc");
+        System.out.println(loginTicket);
     }
 }
